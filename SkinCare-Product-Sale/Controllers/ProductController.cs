bc2df578b5a25f19d085c7e758fb427e86dd05d5
NGUYENPHONGTHINH <phongthinh799@gmail.com>
2025-02-24 10:00:51 +0700

    Product and LayoutAdm

---
+++ SkinCare-Product-Sale/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using Business_Layer.Services.CategoryServices;
+using Business_Layer.Services.ProductServices;
+using Data_Access_Layer.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SkinCare_Product_Sale.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetAllProductsAsync();
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                product.ProductId = Guid.NewGuid().ToString("N");
+                await _productService.AddProductAsync(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+    }
+}
